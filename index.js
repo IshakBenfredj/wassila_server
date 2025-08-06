4d1db4ad825c2dcbd87aa667d6484e21222ae013
IshakBenfredj <ishakbanfradje@gmail.com>
2025-08-06 21:01:31 +0100

    changes

--- index.js
+++ index.js
@@ -56,8 +56,6 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/wassila";
 
 mongoose
   .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     retryWrites: true,
     w: "majority",
   })
