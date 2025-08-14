85a39d57afe98ff66376922d53eead38bc732f50
IshakBenfredj <ishakbanfradje@gmail.com>
2025-08-14 11:40:10 +0100

    changes

--- models/Message.js
+++ models/Message.js
@@ -1,46 +1,3 @@
-// // models/Message.js
-// const mongoose = require('mongoose');
-
-// const messageSchema = new mongoose.Schema({
-//   chat: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Chat',
-//     required: true
-//   },
-//   sender: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true
-//   },
-//   text: {
-//     type: String,
-//     required: true,
-//     trim: true
-//   },
-//   type: {
-//     type: String,
-//     enum: ['text', 'image', 'file'],
-//     default: 'text'
-//   },
-//   read: {
-//     type: Boolean,
-//     default: false
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// }, {
-//   timestamps: true
-// });
-
-// // Indexes for better performance
-// messageSchema.index({ chat: 1, createdAt: -1 });
-// messageSchema.index({ sender: 1 });
-
-// module.exports = mongoose.model('Message', messageSchema);
-
-
 // models/Message.js
 const mongoose = require('mongoose');
 
@@ -69,24 +26,16 @@ const messageSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
-  deletedBy: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    default: []
-  }],
   createdAt: {
     type: Date,
     default: Date.now
   }
 }, {
-  timestamps: true,
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
+  timestamps: true
 });
 
 // Indexes for better performance
 messageSchema.index({ chat: 1, createdAt: -1 });
 messageSchema.index({ sender: 1 });
-messageSchema.index({ deletedBy: 1 });
 
 module.exports = mongoose.model('Message', messageSchema);
